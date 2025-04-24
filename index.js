4ea1df41c32dac8e8d0d07bc8cc78d0e6c9620f7
sandykr29 <sandeepkumar29774@yahoo.com>
2025-04-24 20:37:48 +0530

    assignment 2

---
+++ index.js
@@ -0,0 +1,22 @@
+
+// Question 11 
+
+console.log("Task 1");
+console.log("Task 2");
+console.log("Task 3");
+
+
+// question 12 
+console.log("Message 1");
+
+setTimeout(() => {
+  console.log("Message 2 after 2 seconds");
+}, 2000);
+
+console.log("Message 3");
+
+
+//output would be
+// Message 1
+// Message 3
+// Message 2 after 2 seconds
